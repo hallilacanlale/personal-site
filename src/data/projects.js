125e5170a444a46c1a22cd5c6dbb6abc9e9f0812
Halli Lacanlale <hlacan2@uic.edu>
2023-03-30 15:51:57 -0500

    what is this

--- src/data/projects.js
+++ src/data/projects.js
@@ -35,7 +35,7 @@ const data = [
   },
   {
     title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    subtitle: 'A convolutiossnal neural network to classify cats! (and dogs)',
     image: '/images/projects/catdetector.jpg',
     date: '2015-05-15',
     desc:
