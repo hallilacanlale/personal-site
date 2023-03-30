5b5d045da5384892b7f4beb6dde0b4b1aa3c5591
Halli Lacanlale <hlacan2@uic.edu>
2023-03-30 12:41:17 -0500

    json package update

--- src/components/Resume/Experience/Job.js
+++ src/components/Resume/Experience/Job.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const Job = ({ data }) => (
   <article className="jobs-container">
     <header>
-    <h4>{data.position} - {data.company}</h4>
-    <p className="daterange"> {data.daterange}</p>
+      <h4>{data.position} - {data.company}</h4>
+      <p className="daterange"> {data.daterange}</p>
     </header>
     <ul className="points">
       {data.points.map((point) => (
