d4c6557c3d3e5a5cd003c5571c14d36425af49df
Halli Lacanlale <hlacan2@uic.edu>
2023-03-30 13:07:15 -0500

    please

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -16,7 +16,7 @@ Degree.propTypes = {
     degree: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
     school: PropTypes.string.isRequired,
-    year: PropTypes.number.isRequired,
+    year: PropTypes.string.isRequired,
     honor: PropTypes.string.isRequired,
   }).isRequired,
 };
