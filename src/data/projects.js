258f37719b0fa06a97f2cdbbf1a51763ba4a8b45
Halli Lacanlale <hlacan2@uic.edu>
2023-03-30 13:52:49 -0500

    8

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,8 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Natural Language Processing-based Chatbot',
-    subtitle: '',
+    title: 'Nearest Dollar',
+    subtitle: '2015 BVP Hackathon',
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
     desc:
@@ -12,19 +12,19 @@ const data = [
       + 'the charity of your choice.',
   },
   {
-    title: 'CyberTrade: Stock Bot',
+    title: 'Harvest',
     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://github.com/hallilacanlale/StocksBot',
-    image: '/images/projects/stocks.jpg',
-    date: '2022-03-01',
+    link: 'https://devpost.com/software/harvest',
+    image: '/images/projects/harvest.jpg',
+    date: '2015-09-20',
     desc:
-      'Allows stock traders to buy, sell, and trade stocks automatically using a'
-      + 'bot that executes trades based on a specific trading strategy, with the goal of automating profits. '
-      + 'It utilizes electric components such as Arduino, LED screen and RFID card.',
+      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
+      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
+      + 'and nutrient deficiencies at an affordable price for the developing world.',
   },
   {
-    title: 'AI Pacman',
-    subtitle: '',
+    title: 'Space Potato',
+    subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: 'http://www.spacepotato.org',
     image: '/images/projects/spacepotato.jpg',
     date: '2015-06-28',
@@ -34,15 +34,67 @@ const data = [
       + 'You can email me for a copy.',
   },
   {
-    title: 'Management Utility System',
-    image: '/images/projects/covid-test-website-image.jpg',
-    link: 'https://github.com/hallilacanlale/multithreaded-system/',
-    date: '2022-05-15',
+    title: 'Cat Detector',
+    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    image: '/images/projects/catdetector.jpg',
+    date: '2015-05-15',
     desc:
-      'Implemented a multithreaded system to track COVID diagnostic test using locking '
-      + 'to allow concurrent access without conflicting operations. Allows threads to sample tests '
-      + 'from various testing sites by executing mutual exclusion for an unbounded number of threads',
+      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
+      + 'Over 60,000 cats were classified before server bills made the project too expensive '
+      + 'to continue hosting.',
   },
 ];
 
 export default data;
+
+// // TODO Add a couple lines about each project
+// const data = [
+//   {
+//     title: 'Natural Language Processing-based Chatbot',
+//     subtitle: '',
+//     image: '/images/projects/nearestdollar.jpg',
+//     date: '2015-11-20',
+//     desc:
+//       'Built for a social impact hackathon. '
+//       + 'NearestDollar connected to your bank accounts, credit cards, '
+//       + 'or debit cards and rounded up your purchases to donate the balance to '
+//       + 'the charity of your choice.',
+//   },
+//   {
+//     title: 'CyberTrade: Stock Bot',
+//     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
+//     link: 'https://github.com/hallilacanlale/StocksBot',
+//     image: '/images/projects/stocks.jpg',
+//     date: '2022-03-01',
+//     desc:
+//       'Allows stock traders to buy, sell, and trade stocks automatically using a'
+//       + 'bot that executes trades based on a specific trading strategy, with the goal
+// of automating profits. '
+//       + 'It utilizes electric components such as Arduino, LED screen and RFID card.',
+//   },
+//   {
+//     title: 'AI Pacman',
+//     subtitle: '',
+//     link: 'http://www.spacepotato.org',
+//     image: '/images/projects/spacepotato.jpg',
+//     date: '2015-06-28',
+//     desc:
+//       'Launched a potato battery powered weather balloon with two cameras '
+//       + 'and gps transponder. Resulting photos were published in a coffee table book. '
+//       + 'You can email me for a copy.',
+//   },
+//   {
+//     title: 'Management Utility System',
+//     image: '/images/projects/covid-test-website-image.jpg',
+//     link: 'https://github.com/hallilacanlale/multithreaded-system/',
+//     date: '2022-05-15',
+//     desc:
+//       'Implemented a multithreaded system to track COVID diagnostic test using locking '
+//       + 'to allow concurrent access without conflicting operations.
+//   Allows threads to sample tests '
+//       + 'from various testing sites by executing mutual exclusion for an
+// unbounded number of threads',
+//   },
+// ];
+
+// export default data;
